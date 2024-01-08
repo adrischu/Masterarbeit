534c3f49e1db9c1b38d1252acc69678f29b330c0
Adrian <adrian.schubert@arcor.de>
2024-01-08 16:23:41 +0100

    error fixing

---
+++ src/typescript/classes/InterfaceStatikobjekt.ts
@@ -0,0 +1,20 @@
+export interface isStatikobjekt {
+ Nummer: number
+
+ get values()
+
+ set values(values: any)
+
+ get header(): {
+  title: string //Angezeigter Name im Tabellenkopf
+  unit?: string //Angezeigte Einheit im Tabellenkopf
+  value: string | number | boolean //Angezeigter Wert im Tabellenkörper
+  inputType: string | "fixed" | "input" | "select" | "checkbox" //Eingabetyp
+  tooltip?: string //Tooltip im HTML Format
+  //Für inputType = 'input'
+  inputFormat?: string //Eingabeformat, (nur für inputType=input)
+  //FÜr inputType = 'select'
+  selectListKeys?: any[] //Mitgegebene Liste für Combobox (nur falls inputType=select)
+  selectListValues?: any[] //Liste der jeweiligen Werte zur Anzeige in der Combobox
+ }[]
+}
