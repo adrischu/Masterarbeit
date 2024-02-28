3315755d5bed91ebf5e00eb3f6fdc40405363ebc
Adrian <adrian.schubert@arcor.de>
2024-02-28 17:32:23 +0100

    grafik für vorverformungen implementiert + aufgeräumt

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
    <v-tab value="dev">Dev-Ausgabe</v-tab>
   </v-tabs>
   <span><v-btn @click="systemStore.system.berechnen()">Rechnen</v-btn></span>
-  <span style="color: rgb(128, 128, 128); margin: 0px">current Commit: 25.02.2024 16:42</span>
+  <span style="color: rgb(128, 128, 128); margin: 0px">current Commit: 28.02.2024 17:32</span>
 
   <v-card-text>
    <v-window
